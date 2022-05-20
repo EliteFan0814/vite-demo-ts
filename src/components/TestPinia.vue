094e04255b4c583090aef7fff0579fa31f0b026c
EliteFan0814 <fan552426811@outlook.com>
2022-05-20 14:20:15 +0800

    1 file modified

--- src/components/TestPinia.vue
+++ src/components/TestPinia.vue
@@ -8,7 +8,8 @@ export default {
     return {
       title: '这是用来测试pinia的',
       store: userStore(),
-      ht:'hahahaah'
+      ht: '一种LiMn&lt;sub&gt;2&lt;/sub&gt;O&lt;sub&gt;4&lt;/sub&gt;/掺杂NASICON/Li&lt;sub&gt;4&lt;/sub&gt;Ti&lt;sub&gt;5&lt;/sub&gt;O&lt;sub&gt;12&lt;/sub&gt;全固态薄膜电池及制备方法',
+      ht2: '一种LiMn<sub>2</sub>O<sub>4</sub>/掺杂NASICON/Li<sub>4</sub>Ti<sub>5</sub>O<sub>12</sub>全固态薄膜电池及制备方法',
     };
   },
   computed: {
@@ -36,6 +37,7 @@ export default {
   <div class="test-wrap">
     <h3>{{ title }}</h3>
     <p v-html="ht"></p>
+    <p v-html="ht2"></p>
     <table border="1">
       <tr>
         <th>姓名</th>
